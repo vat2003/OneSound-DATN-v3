810edbfaa6948f77bf77e2f6eb4ade435a4b1154
Pongsua <baokha2923@gmail.com>
2024-01-31 18:03:52 +0700

    xong validate Genres bỏ trống input + tên Genre đã tồn tại

--- Font-end/admin-fontend-DATN-0001/src/app/pages/adminPage/manage/managegenre-admin/managegenre-admin.component.ts
+++ Font-end/admin-fontend-DATN-0001/src/app/pages/adminPage/manage/managegenre-admin/managegenre-admin.component.ts
@@ -1,53 +1,48 @@
-import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
-import {CommonModule, NgOptimizedImage} from "@angular/common";
-import {FormsModule} from "@angular/forms";
-import {Genre} from "../../adminEntityService/adminEntity/genre/genre";
-import {GenreServiceService} from "../../adminEntityService/adminService/genre-service.service";
-import {ActivatedRoute, Router, RouterLink} from '@angular/router';
-import {FirebaseStorageCrudService} from "../../../../services/firebase-storage-crud.service";
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Genre } from '../../adminEntityService/adminEntity/genre/genre';
+import { GenreServiceService } from '../../adminEntityService/adminService/genre-service.service';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { FirebaseStorageCrudService } from '../../../../services/firebase-storage-crud.service';
+import { error, log } from 'console';
 
 @Component({
   selector: 'app-managegenre-admin',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    NgOptimizedImage,
-    RouterLink,
-  ],
+  imports: [CommonModule, FormsModule, NgOptimizedImage, RouterLink],
   templateUrl: './managegenre-admin.component.html',
-  styleUrl: './managegenre-admin.component.scss'
+  styleUrl: './managegenre-admin.component.scss',
 })
 export class ManagegenreAdminComponent implements OnInit {
   id!: number;
   Genre: Genre[] = [];
   Genree: Genre = new Genre();
+  GenreeByName: any;
   imageUrl: string = '';
   setImageUrl: string = '';
   imageFile: any;
-
-  constructor
-  (private GenreService: GenreServiceService,
-   private router: Router,
-   private route: ActivatedRoute,
-   private el: ElementRef,
-   private renderer: Renderer2,
-   private firebaseStorage: FirebaseStorageCrudService,
-  ) {
-  }
+  submitted = false;
+  errorFieldsArr: String[] = [];
+  constructor(
+    private GenreService: GenreServiceService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private firebaseStorage: FirebaseStorageCrudService
+  ) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
-    this.getEmployees();
+    this.getListGenresPage();
     this.loadSingerById();
     this.getGenre(this.id);
-
-
   }
 
   onFileSelected(event: any) {
     const archivoSelectcionado: File = event.target.files[0];
-    console.log("FILE OBJECT ==> ", archivoSelectcionado);
+    console.log('FILE OBJECT ==> ', archivoSelectcionado);
     if (archivoSelectcionado) {
       const reader = new FileReader();
       reader.onload = (e: any) => {
@@ -64,9 +59,21 @@ export class ManagegenreAdminComponent implements OnInit {
   }
 
   fillImage(url: string): void {
-    this.renderer.setStyle(this.el.nativeElement.querySelector('.image-upload-wrap'), 'display', 'none');
-    this.renderer.setAttribute(this.el.nativeElement.querySelector('.file-upload-image'), 'src', url);
-    this.renderer.setStyle(this.el.nativeElement.querySelector('.file-upload-content'), 'display', 'block');
+    this.renderer.setStyle(
+      this.el.nativeElement.querySelector('.image-upload-wrap'),
+      'display',
+      'none'
+    );
+    this.renderer.setAttribute(
+      this.el.nativeElement.querySelector('.file-upload-image'),
+      'src',
+      url
+    );
+    this.renderer.setStyle(
+      this.el.nativeElement.querySelector('.file-upload-content'),
+      'display',
+      'block'
+    );
     if (url.length == 0) {
       this.removeUpload();
     }
@@ -74,9 +81,21 @@ export class ManagegenreAdminComponent implements OnInit {
 
   removeUpload(): void {
     this.imageUrl = '';
-    this.renderer.setProperty(this.el.nativeElement.querySelector('.file-upload-input'), 'value', '');
-    this.renderer.setStyle(this.el.nativeElement.querySelector('.file-upload-content'), 'display', 'none');
-    this.renderer.setStyle(this.el.nativeElement.querySelector('.image-upload-wrap'), 'display', 'block');
+    this.renderer.setProperty(
+      this.el.nativeElement.querySelector('.file-upload-input'),
+      'value',
+      ''
+    );
+    this.renderer.setStyle(
+      this.el.nativeElement.querySelector('.file-upload-content'),
+      'display',
+      'none'
+    );
+    this.renderer.setStyle(
+      this.el.nativeElement.querySelector('.image-upload-wrap'),
+      'display',
+      'block'
+    );
   }
 
   loadSingerById() {
@@ -88,21 +107,18 @@ export class ManagegenreAdminComponent implements OnInit {
     );
   }
 
+  getListGenresPage() {
+    this.GenreService.getListGenres(0, 10).subscribe(async (data) => {
+      console.log(data);
+      this.Genre = data.content;
 
-  getEmployees() {
-    this.GenreService.getCategories(0, 10).subscribe(
-      async data => {
-        console.log(data);
-        this.Genre = data.content;
-
-        for (const genre of this.Genre) {
-          if (genre.image == "" || genre.image == null) {
-            continue;
-          }
-          genre.image = await this.setImageURLFirebase(genre.image);
+      for (const genre of this.Genre) {
+        if (genre.image == '' || genre.image == null) {
+          continue;
         }
-      });
-
+        genre.image = await this.setImageURLFirebase(genre.image);
+      }
+    });
   }
 
   async setImageURLFirebase(image: string): Promise<string> {
@@ -114,31 +130,46 @@ export class ManagegenreAdminComponent implements OnInit {
   }
 
   goToSingerList() {
-    this.getEmployees();
+    this.getListGenresPage();
     this.router.navigate(['/manage/genre']);
   }
 
   saveGenre() {
     this.Genree.image = this.setImageUrl;
+    this.errorFieldsArr = this.validateGenreEmpty(this.Genree);
+    if (this.errorFieldsArr.length !== 0) {
+      return;
+    }
+    if (this.isNameExistsInArray(this.Genree)) {
+      alert('The name of the music genre already exists');
+      this.errorFieldsArr.push('existGenreName');
+      return;
+    }
+
     this.GenreService.createGenre(this.Genree).subscribe(
       async (data) => {
         if (this.Genree.image != null) {
-          await this.firebaseStorage.uploadFile('adminManageImage/genre/', this.imageFile);
+          await this.firebaseStorage.uploadFile(
+            'adminManageImage/genre/',
+            this.imageFile
+          );
         }
         this.goToSingerList();
         console.log(data);
       },
       (error) => console.log(error)
     );
-
   }
 
-  updateGender(id: number) {
+  updateGenre(id: number) {
     this.Genree.image = this.setImageUrl;
     this.GenreService.updateGenre(id, this.Genree).subscribe(
       async (data) => {
         if (this.Genree.image != null && this.Genree.image != 'null') {
-          await this.firebaseStorage.uploadFile('adminManageImage/genre/', this.imageFile);
+          await this.firebaseStorage.uploadFile(
+            'adminManageImage/genre/',
+            this.imageFile
+          );
         }
         this.goToSingerList();
         this.Genree = new Genre();
@@ -148,7 +179,6 @@ export class ManagegenreAdminComponent implements OnInit {
       },
       (error) => console.log(error)
     );
-
   }
 
   getGenre(id: number) {
@@ -160,16 +190,18 @@ export class ManagegenreAdminComponent implements OnInit {
       (error: any) => {
         console.log(error);
       }
-    )
+    );
     // this.Genree = this.router.navigate(['onesound/admin/manage/genre/', id]);
   }
 
   deleteGender(id: number) {
-    const isConfirmed = window.confirm('Are you sure you want to delete this singer?');
+    const isConfirmed = window.confirm(
+      'Are you sure you want to delete this singer?'
+    );
     if (isConfirmed) {
       this.GenreService.deleteGenre(id).subscribe((data) => {
         console.log(data);
-        this.getEmployees();
+        this.getListGenresPage();
       });
     }
   }
@@ -182,5 +214,22 @@ export class ManagegenreAdminComponent implements OnInit {
     // }
   }
 
+  validateGenreEmpty(valueCheck: any): string[] {
+    const errorFieldsArr: string[] = [];
+    for (const key in valueCheck) {
+      if (valueCheck.hasOwnProperty(key)) {
+        if (!valueCheck[key]) {
+          //valueCheck[key] là giá trị
+          //key là tên thuộc tính
+          errorFieldsArr.push(key);
+        }
+      }
+    }
+    //return nếu không lỗi
+    return errorFieldsArr;
+  }
 
+  isNameExistsInArray(genreCheck: Genre): boolean {
+    return this.Genre.some((genre) => genre.name === genreCheck.name);
+  }
 }
