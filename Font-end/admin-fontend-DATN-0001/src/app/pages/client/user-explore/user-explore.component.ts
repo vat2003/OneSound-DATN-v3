6a94b84c3370385a695fbd62248277f9e5eb6209
Pongsua <baokha2923@gmail.com>
2024-02-20 09:33:13 +0700

    đổ dữ liệu hot artist lên clietn

--- Font-end/admin-fontend-DATN-0001/src/app/pages/client/user-explore/user-explore.component.ts
+++ Font-end/admin-fontend-DATN-0001/src/app/pages/client/user-explore/user-explore.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SingerService } from '../../adminPage/adminEntityService/adminService/singer-service.service';
+import { Singer } from '../../adminPage/adminEntityService/adminEntity/singer/singer';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FirebaseStorageCrudService } from '../../../services/firebase-storage-crud.service';
 
 @Component({
   selector: 'app-user-explore',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user-explore.component.html',
-  styleUrl: './user-explore.component.scss'
+  styleUrl: './user-explore.component.scss',
 })
-export class UserExploreComponent {
+export class UserExploreComponent implements OnInit {
+  hotArtist: Singer[] = [];
+  constructor(
+    private SingerService: SingerService,
+    private firebaseStorage: FirebaseStorageCrudService
+  ) {}
+  ngOnInit(): void {
+    this.getAllArtist();
+  }
 
+  getAllArtist(): void {
+    this.SingerService.getAllArtists().subscribe(async (data) => {
+      this.hotArtist = data;
+      console.log(this.hotArtist);
+
+      debugger;
+      console.log(data);
+      this.hotArtist = data;
+
+      for (const hotArt of this.hotArtist) {
+        if (hotArt.image == null || hotArt.image == '') {
+          continue;
+        }
+        hotArt.image = await this.setImageURLFirebase(hotArt.image);
+      }
+      // console.log(data);
+    });
+  }
+  async setImageURLFirebase(image: string): Promise<string> {
+    if (image != null) {
+      return await this.firebaseStorage.getFile(image);
+    } else {
+      return 'null';
+    }
+  }
 }
