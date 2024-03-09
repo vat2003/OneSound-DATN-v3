5332341a413a478bfabda63aad945522f9c0ee7f
vat2003 <anhtaivo43@gmail.com>
2024-03-09 08:58:05 +0700

    a

--- Font-end/admin-fontend-DATN-0001/src/app/pages/client/user-playlist-modal/user-playlist-modal.component.ts
+++ Font-end/admin-fontend-DATN-0001/src/app/pages/client/user-playlist-modal/user-playlist-modal.component.ts
@@ -3,6 +3,8 @@ import {FormsModule} from "@angular/forms";
 import {NgIf} from "@angular/common";
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 import {Song} from "../../adminPage/adminEntityService/adminEntity/song/song";
+import {account} from "../../adminPage/adminEntityService/adminEntity/account/account";
+import {accountServiceService} from "../../adminPage/adminEntityService/adminService/account-service.service";
 
 @Component({
   selector: 'app-user-playlist-modal',
@@ -15,12 +17,15 @@ import {Song} from "../../adminPage/adminEntityService/adminEntity/song/song";
   styleUrl: './user-playlist-modal.component.scss'
 })
 export class UserPlaylistModalComponent implements OnInit {
-
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { song: Song }) {
+  account?: account | null;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { song: Song },
+              private userService: accountServiceService,
+  ) {
   }
 
 
   ngOnInit(): void {
+    this.account = this.userService.getUserResponseFromLocalStorage();
     console.log(this.data.song.name)
   }
 
