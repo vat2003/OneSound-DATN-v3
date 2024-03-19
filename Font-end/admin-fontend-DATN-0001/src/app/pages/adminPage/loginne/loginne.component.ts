222004a0d7c065e40ab18f50bae8fa7bd0c20dcb
Benlaptrinh <nguyenhuuviet159@gmail.com>
2024-03-19 11:12:27 +0700

    a

--- Font-end/admin-fontend-DATN-0001/src/app/pages/adminPage/loginne/loginne.component.ts
+++ Font-end/admin-fontend-DATN-0001/src/app/pages/adminPage/loginne/loginne.component.ts
@@ -1,20 +1,19 @@
-import {CommonModule} from '@angular/common';
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {FormsModule, NgForm, Validators} from '@angular/forms';
-import {Router, RouterLink} from '@angular/router';
-import {account} from '../adminEntityService/adminEntity/account/account';
-import {login} from '../adminEntityService/adminEntity/DTO/login';
-import {accountServiceService} from '../adminEntityService/adminService/account-service.service';
-import {TokenService} from '../adminEntityService/adminService/token.service';
-import {LoginResponse} from '../adminEntityService/adminEntity/utils/login.response';
-import {Register} from '../adminEntityService/adminEntity/DTO/Register';
-import {routes} from '../../../app.routes';
-import {NgToastModule, NgToastService} from "ng-angular-popup";
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, NgForm, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { account } from '../adminEntityService/adminEntity/account/account';
+import { login } from '../adminEntityService/adminEntity/DTO/login';
+import { accountServiceService } from '../adminEntityService/adminService/account-service.service';
+import { TokenService } from '../adminEntityService/adminService/token.service';
+import { LoginResponse } from '../adminEntityService/adminEntity/utils/login.response';
+import { Register } from '../adminEntityService/adminEntity/DTO/Register';
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-loginne',
   standalone: true,
-  imports: [RouterLink, CommonModule, FormsModule, NgToastModule],
+  imports: [RouterLink, CommonModule, FormsModule],
   // providers: [
   //     SocialAuthService,
   //   {
@@ -54,10 +53,9 @@ export class LoginneComponent implements OnInit {
   constructor(
     private router: Router,
     private userService: accountServiceService,
-    private tokenService: TokenService, private toast: NgToastService
+    private tokenService: TokenService
   ) // private authService: SocialAuthService,
-  {
-  }
+  {}
 
   ngOnInit(): void {
     this.account = this.userService.getUserResponseFromLocalStorage();
@@ -67,12 +65,12 @@ export class LoginneComponent implements OnInit {
   }
 
   // loginWithFacebook(): void {
-  //   debugger
+  //  
   //   this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
 
   //   // Subscribe to authState observable
   //   this.authService.authState.subscribe((user) => {
-  //     debugger
+  //    
   //     console.log(user) + "<----------------";
   //     console.log(user) + "<----------------";
   //     console.log(user) + "<----------------";
@@ -104,7 +102,7 @@ export class LoginneComponent implements OnInit {
   // }
 
   // loginWithGoogle(): void {
-  //   debugger
+  //  
   //   this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
   // }
 
@@ -120,60 +118,86 @@ export class LoginneComponent implements OnInit {
       email: this.email,
       password: this.password,
     };
-    debugger;
-    this.userService.login(login).subscribe({
-      next: (response: LoginResponse) => {
-        alert('Login successful!');
-
-        const {token} = response;
-        console.log(token);
-        this.tokenService.setToken(token);
-        this.incorrectLoginAttempts = 0;
-
-        this.userService.getUserDetail(token).subscribe({
-          next: (response: any) => {
-            debugger;
-            this.account = {
-              ...response,
-            };
-            console.log(response);
-            this.userService.saveUserResponseToLocalStorage(response);
-            if (this.account && this.account.accountRole) {
-              debugger;
-              alert(this.account.accountRole.name);
-
-              if (this.account.accountRole.name === 'admin') {
-                debugger;
-                this.router.navigate(['/onesound/admin']);
-              } else if (this.account.accountRole.name === 'user') {
-                debugger;
-                this.router.navigate(['/onesound/home/explore']);
+    debugger
+    this.userService.checkactive1(login.email).subscribe({
+     
+      next: (data) => {
+        debugger
+        if (data != null) {
+          debugger
+          console.log(data);
+          this.userService.login(login).subscribe({
+            next: (response: LoginResponse) => {
+              alert('Login successful!');
+      
+              const { token } = response;
+              console.log(token);
+              this.tokenService.setToken(token);
+              this.incorrectLoginAttempts = 0;
+      
+              this.userService.getUserDetail(token).subscribe({
+                
+                next: (response: any) => {
+                  
+                  this.account = {
+                    ...response,
+                  };
+                  console.log(response);
+                  this.userService.saveUserResponseToLocalStorage(response);
+                  if (this.account && this.account.accountRole) {
+                    
+                    alert(this.account.accountRole.name);
+      
+                    if (this.account.accountRole.name === 'admin') {
+                      
+                      this.router.navigate(['/onesound/admin']);
+                    } else if (this.account.accountRole.name === 'user') {
+                      
+                      this.router.navigate(['/onesound/home/explore']);
+                    }
+                  } else {
+                    
+                    alert('Error: User role information not found.');
+                  }
+                },
+                complete: () => {},
+                error: (error: any) => {
+                  console.log(error);
+                },
+              });
+            },
+            error: (error) => {
+              console.error(error);
+              alert('Wrong password, please check again');
+              this.incorrectLoginAttempts++;
+      
+              if (this.incorrectLoginAttempts >= this.maxIncorrectLoginAttempts) {
+                alert('Too many incorrect login attempts. Please try again later.');
+                this.router.navigate(['/onesound/dangky']);
               }
-            } else {
-              debugger;
-              alert('Error: User role information not found.');
-            }
-          },
-          complete: () => {
-          },
-          error: (error: any) => {
-            console.log(error);
-          },
-        });
+            },
+            complete: () => {},
+          });
+        }else{
+          alert("Your account has been locked" )
+
+        }
+       
       },
       error: (error) => {
-        console.error(error);
-        alert('Wrong password, please check again');
-        this.incorrectLoginAttempts++;
-
-        if (this.incorrectLoginAttempts >= this.maxIncorrectLoginAttempts) {
-          alert('Too many incorrect login attempts. Please try again later.');
-          this.router.navigate(['/onesound/dangky']);
-        }
+        // Xử lý lỗi khi yêu cầu gặp vấn đề
+        alert("Your account has been locked" )
+        // Thực hiện các hành động phù hợp với lỗi nhận được
       },
       complete: () => {
-      },
+        // Xử lý khi yêu cầu hoàn thành (không bắt buộc)
+        console.log('Yêu cầu hoàn thành');
+        // Thực hiện các hành động phù hợp khi yêu cầu hoàn thành
+      }
     });
+    
+
+ 
     // } else {
     //   alert("Email does not exist. Please check or register.");
     // }
@@ -184,83 +208,6 @@ export class LoginneComponent implements OnInit {
     //   }
     // });
   }
-  // login() {
-  //   this.userService.checkEmailExists(this.email).subscribe({
-  //     next: (emailExists: boolean) => {
-  //       if (emailExists) {
-  //         var login: login = {
-  //           email: this.email,
-  //           password: this.password,
-  //         };
-  //         debugger
-  //         this.userService.login(login).subscribe({
-
-  //           next: (response: LoginResponse) => {
-
-  //             alert("Login successful!");
-
-  //             const {token} = response;
-  //             console.log(token);
-  //             this.tokenService.setToken(token);
-  //             this.incorrectLoginAttempts = 0;
-
-  //             this.userService.getUserDetail(token).subscribe({
-
-  //               next: (response: any) => {
-  //                 debugger
-  //                 this.account = {
-  //                   ...response
-  //                 };
-  //                 console.log(response);
-  //                 this.userService.saveUserResponseToLocalStorage(response);
-  //                 if (this.account && this.account.accountRole) {
-  //                   debugger
-  //                   alert(this.account.accountRole.name);
-
-  //                   if (this.account.accountRole.name === 'admin') {
-  //                     debugger
-  //                     this.router.navigate(['/onesound/admin']);
-  //                   } else if (this.account.accountRole.name === 'user') {
-  //                     debugger
-  //                     this.router.navigate(['/onesound/home/explore']);
-
-  //                   }
-  //                 } else {
-  //                   debugger
-  //                   alert("Error: User role information not found.");
-  //                 }
-  //               },
-  //               complete: () => {
-  //               },
-  //               error: (error: any) => {
-  //                 console.log(error);
-  //               }
-  //             });
-
-  //           },
-  //           error: (error) => {
-  //             console.error(error);
-  //             alert("Wrong password, please check again");
-  //             this.incorrectLoginAttempts++;
-
-  //             if (this.incorrectLoginAttempts >= this.maxIncorrectLoginAttempts) {
-  //               alert("Too many incorrect login attempts. Please try again later.");
-  //               this.router.navigate(['/onesound/dangky']);
-  //             }
-  //           },
-  //           complete: () => {
-  //           }
-  //         });
-  //       } else {
-  //         alert("Email does not exist. Please check or register.");
-  //       }
-  //     },
-  //     error: (error) => {
-  //       console.error(error);
-  //       alert("Please do not leave blank");
-  //     }
-  //   });
-  // }
 
   loginByGoogle() {
     window.location.href = 'http://localhost:8080/oauth2/authorization/google';
